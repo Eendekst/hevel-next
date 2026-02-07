8ab2f1ec6cd6df70853c4263dacd68a049ef4c56
Hevel <jason@hevel.ca>
2026-02-07 03:32:37 -0500

    chore(config): update social links (ig: hevelshow, tiktok: hevelstudio)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 export const SOCIAL_LINKS = {
-  instagram: 'https://www.instagram.com/heveldealstore/',
+  instagram: 'https://www.instagram.com/hevelshow/',
   reddit: 'https://www.reddit.com/r/Brandfessionals/',
   youtube: 'https://www.youtube.com/@HevelProd',
   medium: 'https://medium.com/@jason_66813',
-  tiktok: 'https://www.tiktok.com/@rickygattuzo',
+  tiktok: 'https://www.tiktok.com/@hevelstudio',
   x: 'https://x.com/JasonHevel',
   pinterest: 'https://ca.pinterest.com/hevelinsights/',
   google: 'https://www.google.com/search?q=Hevel&stick=H4sIAAAAAAAA_-NgU1I1qDBJTrY0TDMwNzQwskg2SEqxMqgwMks0TLM0MzMwSjY3sLBMWsTK6pFalpoDAFEKzsAxAAAA&hl=en-US&mat=CVmuRJ8TkPBVElcBTVDHnp6DMrxLfi8xo3lInN3NYPGgjTvVlLGqLAC8MM6wiRBCsrC5vm5Zp5oLv8AWlMNqOhyVYL1JrdrPEF1N2iJRNM7n2jPEIVMUhL4OQt3Y8Z5JGVU&authuser=0'
